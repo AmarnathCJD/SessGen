8c857822eb9d3a8193a95cccf44eb38123058fee
AmarnathCJD <amarnathcharichilil@gmail.com>
2025-01-16 21:27:03 +0530

    initial commit for testing if vercel supports.

---
+++ api/gen.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+
+	tg "github.com/amarnathcjd/gogram/telegram"
+)
+
+func SendCodeHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	appId := r.Form.Get("appId")
+	appHash := r.Form.Get("appHash")
+	phone := r.Form.Get("phoneNumber")
+
+	if appId == "" || appHash == "" || phone == "" {
+		http.Error(w, "appId, appHash and phoneNumber are required", http.StatusBadRequest)
+		return
+	}
+
+	appIdInt, err := strconv.Atoi(appId)
+	if err != nil {
+		http.Error(w, "appId must be an integer", http.StatusBadRequest)
+		return
+	}
+
+	client, err := tg.NewClient(tg.ClientConfig{
+		AppID:         int32(appIdInt),
+		AppHash:       appHash,
+		MemorySession: true,
+	})
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = client.Connect()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module main
+
+go 1.23.2
+
+require github.com/amarnathcjd/gogram v1.5.3
+
+require (
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/net v0.31.0 // indirect
+)
